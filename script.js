ca75541faef282d2ad51a0130b976111463f2949
Rhuan Nascimento Ferreria <rhuan2414@gmail.com>
2022-04-28 15:59:44 -0300

    O remover do lipe ja esta funcionando, adicionar aparecendo em todas listas ja passando parametro pra usar dps no remover delas

--- script.js
+++ script.js
@@ -3,6 +3,42 @@ let rhuanRecebe = {lipeDeve:[], gianDeve:[]};
 let lipeRecebe = {gianDeve:[], rhuanDeve:[]};
 
 
+let removeritem = document.querySelector('#remover')
+
+removeritem.addEventListener('click', () => {
+
+        let llista = document.querySelector('#lipeRecebe')
+        let rlista = document.querySelector('#rhuanRecebe')
+        let glista = document.querySelector('#gianRecebe')
+
+        if (llista.options[llista.selectedIndex] != null ){
+            if(llista.options[llista.selectedIndex].text.includes('Gian')){
+                alert(`O valor de que o gian devia era: ${lipeRecebe.gianDeve[Number(llista.options[llista.selectedIndex].value)]}`)
+                lipeRecebe.gianDeve[Number(llista.options[llista.selectedIndex].value)] = 0
+                alert(`O valor de que o gian deve agora e: ${lipeRecebe.gianDeve[Number(llista.options[llista.selectedIndex].value)]}`)
+            }
+            if(llista.options[llista.selectedIndex].text.includes('Rhuan')){
+                alert(`O valor de que o rhuan devia era: ${lipeRecebe.rhuanDeve[Number(llista.options[llista.selectedIndex].value)]}`)
+                lipeRecebe.rhuanDeve[Number(llista.options[llista.selectedIndex].value)] = 0
+                alert(`O valor de que o rhuan deve agore e: ${lipeRecebe.rhuanDeve[Number(llista.options[llista.selectedIndex].value)]}`)
+            }
+
+            llista.removeChild(llista.options[llista.selectedIndex])
+            
+
+        }else if (glista.options[glista.selectedIndex] != null ){
+            glista.removeChild(glista.options[glista.selectedIndex])
+            
+        }
+
+
+        //let x = document.querySelectorAll('.selecao')
+        //let selecionado = x.options[x.selectedIndex]
+        //x.removeChild(selecionado)
+})
+
+
+
 function adicionar(){
     let valor = document.querySelector('#valor').value
     let ldividir = document.querySelector('input#liped')
@@ -20,14 +56,20 @@ function adicionar(){
             lipeRecebe.rhuanDeve.push(divisao)
 
             let item = document.createElement('option')
-            item.text = `Recebe ${divisao} de Gian e de Rhuan.`
+            let item2 = document.createElement('option')
+            item.value = lipeRecebe.gianDeve.length-1
+            item2.value = lipeRecebe.rhuanDeve.length-1
+            item.text = `Recebe ${divisao} de Gian.`
+            item2.text = `Recebe ${divisao} de Rhuan.`
             llista.appendChild(item)
+            llista.appendChild(item2)
 
         }else if(ldividir.checked && gdividir.checked){
             let divisao = Number(valor)/2
             lipeRecebe.gianDeve.push(divisao)
 
             let item = document.createElement('option')
+            item.value = lipeRecebe.gianDeve.length-1
             item.text = `Recebe ${divisao} de Gian.`
             llista.appendChild(item)
 
@@ -36,6 +78,7 @@ function adicionar(){
             lipeRecebe.rhuanDeve.push(divisao)
 
             let item = document.createElement('option')
+            item.value = lipeRecebe.rhuanDeve.length-1
             item.text = `Recebe ${divisao} de Rhuan.`
             llista.appendChild(item)
 
@@ -45,8 +88,13 @@ function adicionar(){
             lipeRecebe.gianDeve.push(divisao)
 
             let item = document.createElement('option')
-            item.text = `Recebe ${divisao} de Gian e de Rhuan.`
+            let item2 = document.createElement('option')
+            item.value = lipeRecebe.gianDeve.length-1
+            item2.value = lipeRecebe.rhuanDeve.length-1
+            item.text = `Recebe ${divisao} de Gian.`
+            item2.text = `Recebe ${divisao} de Rhuan.`
             llista.appendChild(item)
+            llista.appendChild(item2)
             
         }
     }else if(pagar[1].checked){
@@ -54,32 +102,97 @@ function adicionar(){
             let divisao = Number(valor)/3
             gianRecebe.lipeDeve.push(divisao)
             gianRecebe.rhuanDeve.push(divisao)
+
+            let item = document.createElement('option')
+            let item2 = document.createElement('option')
+            item.value = gianRecebe.lipeDeve.length-1
+            item2.value = gianRecebe.rhuanDeve.length-1
+            item.text = `Recebe ${divisao} de Lipe.`
+            item2.text = `Recebe ${divisao} de Rhuan.`
+            glista.appendChild(item)
+            glista.appendChild(item2)
+
         }else if(gdividir.checked && ldividir.checked){
             let divisao = Number(valor)/2
             gianRecebe.lipeDeve.push(divisao)
+
+            let item = document.createElement('option')
+            item.value = gianRecebe.lipeDeve.length-1
+            item.text = `Recebe ${divisao} de Lipe.`
+            glista.appendChild(item)
+
         }else if(gdividir.checked && rdividir.checked){
             let divisao = Number(valor)/2
             gianRecebe.rhuanDeve.push(divisao)
+
+            let item = document.createElement('option')
+            item.value = gianRecebe.rhuanDeve.length-1
+            item.text = `Recebe ${divisao} de Rhuan.`
+            glista.appendChild(item)
+
         }else{
             let divisao = Number(valor)/2
             gianRecebe.rhuanDeve.push(divisao)
             gianRecebe.lipeDeve.push(divisao)
+
+            let item = document.createElement('option')
+            let item2 = document.createElement('option')
+            item.value = gianRecebe.lipeDeve.length-1
+            item2.value = gianRecebe.rhuanDeve.length-1
+            item.text = `Recebe ${divisao} de Lipe.`
+            item2.text = `Recebe ${divisao} de Rhuan.`
+            glista.appendChild(item)
+            glista.appendChild(item2)
+    
         }
     }else{
         if(rdividir.checked && ldividir.checked && gdividir.checked){
             let divisao = Number(valor)/3
             rhuanRecebe.lipeDeve.push(divisao)
             rhuanRecebe.gianDeve.push(divisao)
+
+            let item = document.createElement('option')
+            let item2 = document.createElement('option')
+            item.value = rhuanRecebe.lipeDeve.length-1
+            item2.value = rhuanRecebe.gianDeve.length-1
+            item.text = `Recebe ${divisao} de Lipe.`
+            item2.text = `Recebe ${divisao} de Gian.`
+            rlista.appendChild(item)
+            rlista.appendChild(item2)
+
         }else if(rdividir.checked && ldividir.checked){
             let divisao = Number(valor)/2
             rhuanRecebe.lipeDeve.push(divisao)
+
+            let item = document.createElement('option')
+            item.value = rhuanRecebe.lipeDeve.length-1
+            item.text = `Recebe ${divisao} de Lipe.`
+            rlista.appendChild(item)
+
+
         }else if(rdividir.checked && gdividir.checked){
             let divisao = Number(valor)/2
             rhuanRecebe.gianDeve.push(divisao)
+
+            let item = document.createElement('option')
+            item.value = rhuanRecebe.gianDeve.length-1
+            item.text = `Recebe ${divisao} de Gian.`
+            rlista.appendChild(item)
+
         }else{
             let divisao = Number(valor)/2
             rhuanRecebe.gianDeve.push(divisao)
             rhuanRecebe.lipeDeve.push(divisao)
+
+            let item = document.createElement('option')
+            let item2 = document.createElement('option')
+            item.value = rhuanRecebe.lipeDeve.length-1
+            item2.value = rhuanRecebe.gianDeve.length-1
+            item.text = `Recebe ${divisao} de Lipe.`
+            item2.text = `Recebe ${divisao} de Gian.`
+            rlista.appendChild(item)
+            rlista.appendChild(item2)
+
         }
     }
 
